c063a7155513ef2534ff70ebe1e45f657158c35d
Cory Wang <cory.wang11@gmail.com>
2021-07-22 10:58:41 -0700

    Added game logic functionality

--- Arborivia/app/src/main/java/com/codeacademy/arborivia/MainActivity.java
+++ Arborivia/app/src/main/java/com/codeacademy/arborivia/MainActivity.java
@@ -22,6 +22,33 @@ public class MainActivity {
     int totalQuestions = 0;
     // Add ArrayList member variable
     ArrayList<Question> questions;
+
+    // TODO 3-A: Declare View member variables
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO 2-G: Show app icon in ActionBar
+
+        setContentView(R.layout.activity_main);
+
+        // TODO 3-B: assign View member variables
+
+        // TODO 4-E: set onClickListener for each answer Button
+
+        // TODO 5-A: set onClickListener for the submit answer Button
+
+        startNewGame();
+    }
+
+    // TODO 3-F: displayQuestion(Question question) {...}
+
+    // TODO 3-C: displayQuestionsRemaining(int questionRemaining) {...}
+
+    // TODO 4-A: onAnswerSelected(int answerSelected) {...}
+
+
     // Method to be called when starting a new game
     public void startNewGame() {
         Question question1 = new Question(1, "Which American Task Force(s) took part in the Battle of Midway?", "Task Force 6", "Task Forces 1 & 3", "Task Force 33", "Task Forces 16 & 17", 3);
@@ -32,31 +59,41 @@ public class MainActivity {
         questions.add(question2);
         questions.add(question3);
         totalCorrect = 0;
-        totalQuestions = 0;
+        totalQuestions = questions.size();
         Question firstQuestion = chooseNewQuestion();
-        // displayQuestion(firstQuestion);
+        // TODO 3-D.ii: Uncomment the line below after implementing displayQuestionsRemaining(int)
         // displayQuestionsRemaining(questions.size());
+
+        // TODO 3-H.ii: Uncomment after implementing displayQuestion(Question)
+        // displayQuestion(firstQuestion);
     }
 
     // Method of randomly choosing questions from the ArrayList in startNewGame()
     public Question chooseNewQuestion() {
-        int newQuestionNumber = generateRandomNumber(2);
+        int newQuestionNumber = generateRandomNumber(questions.size());
         currentQuestionIndex = newQuestionNumber;
         return questions.get(currentQuestionIndex);
     }
     // Convenience method for calling curretly selected question object
     public Question getCurrentQuestion() {
-        return questions.get(currentQuestionIndex);
+        Question currentQuestion = questions.get(currentQuestionIndex);
+        return currentQuestion;
     }
-    // TODO #6 add onAnswerSubmission() here
+
     public void onAnswerSubmission() {
-        if ( getCurrentQuestion().isCorrect() ) {
+        Question currentQuestion = getCurrentQuestion();
+        if ( currentQuestion.isCorrect() ) {
             totalCorrect ++;
         }
-        questions.remove(currentQuestionIndex);
+        questions.remove(currentQuestion);
+        // TODO 3-D.i: Uncomment the line below after implementing displayQuestionsRemaining(int)
         // displayQuestionsRemaining(questions.size());
+
         if (questions.size() == 0) {
-            System.out.println(getGameOverMessage(totalCorrect, totalQuestions));
+            String gameOverMan = getGameOverMessage(totalCorrect, totalQuestions);
+
+            // TODO 5-D: Show a popup instead
+            System.out.println(gameOverMan);
             startNewGame();
         } else {
             chooseNewQuestion();
