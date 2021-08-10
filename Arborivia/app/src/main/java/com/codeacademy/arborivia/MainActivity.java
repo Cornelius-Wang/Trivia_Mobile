cd5cfc708e3689c31fb5a0892833ebd7e8a6d375
Cory Wang <cory.wang11@gmail.com>
2021-08-09 17:09:46 -0700

    Added alerts and question randomization. Adding selection screen.

--- Arborivia/app/src/main/java/com/codeacademy/arborivia/MainActivity.java
+++ Arborivia/app/src/main/java/com/codeacademy/arborivia/MainActivity.java
@@ -85,8 +85,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // TODO 5-A: set onClickListener for the submit answer Button
-
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -141,6 +139,16 @@ public class MainActivity extends AppCompatActivity {
         Question question4 = new Question(R.drawable.img_quote_3, "Insanely inspiring, insanely incorrect(maybe). Who is the true source of this inspiration?", "Nelson Mandela", "Harriet Tubman", "Mahatma Gandhi", "Nicholas Klein", 3);
         Question question5 = new Question(R.drawable.img_quote_4, "A peace worth striving for — who actually reminded us of this?", "Malala Yousafzai", "Martin Luther King Jr.", "Liu Xiaobo", "Dalai Lama", 3);
         Question question6 = new Question(R.drawable.img_quote_5, "Unfortunately, true — but did Marilyn Monroe convey it or did someone else?", "Laurel Thatcher Ulrich", "Eleanor Roosevelt", "Mahatma Gandhi", "Queen Victoria", 0);
+        Question question7 = new Question(R.drawable.img_quote_6, "Here’s the truth, Will Smith did say this, but in which movie?", "Independence Day", "Bad Boys", "Men In Black", "The Pursuit of Happyness", 2);
+        Question question8 = new Question(R.drawable.img_quote_7, "Which TV funny gal actually quipped this 1-liner?", "Ellen Degeneres", "Amy Poehler", "Betty White", "Tina Fay", 3);
+        Question question9 = new Question(R.drawable.img_quote_8, "This mayor won’t get my vote — but did he actually give this piece of advice? And if not, who did?", "Forrest Gump, Forrest Gump", "Dorry, Finding Nemo", "Esther Williams", "The Mayor, Jaws", 1);
+        Question question10 = new Question(R.drawable.img_quote_9, "Her heart will go on, but whose heart is it?", "Whitney Houston", "Diana Ross", "Celine Dion", "Mariah Carey", 0);
+        Question question11 = new Question(R.drawable.img_quote_10, "He’s the king of something alright — to whom does this self-titling line belong to?", "Tony Montana, Scarface", "Joker, The Dark Knight", "Lex Luthor, Batman v Superman", "Jack, Titanic", 3);
+        Question question12 = new Question(R.drawable.img_quote_11, "Is “Grey” synonymous for“wise”? If so, maybe Gandalf did preach this advice. If not, who did?", "Yoda, Star Wars", "Gandalf The Grey, Lord of the Rings", "Dumbledore, Harry Potter", "Uncle Ben, Spider-Man", 0);
+        Question question13 = new Question(R.drawable.img_quote_12, "Houston, we have a problem with this quote — which space-traveler does this catch-phrase actually belong to?", "Han Solo, Star Wars", "Captain Kirk, Star Trek", "Buzz Lightyear, Toy Story", "Jim Lovell, Apollo 13", 2);
+
+
+
         questions = new ArrayList<>();
         questions.add(question1);
         questions.add(question2);
@@ -148,6 +156,19 @@ public class MainActivity extends AppCompatActivity {
         questions.add(question4);
         questions.add(question5);
         questions.add(question6);
+        questions.add(question7);
+        questions.add(question8);
+        questions.add(question9);
+        questions.add(question10);
+        questions.add(question11);
+        questions.add(question12);
+        questions.add(question13);
+
+        while (questions.size() > 6) {
+            int q = generateRandomNumber(questions.size());
+            questions.remove(q);
+        }
+
         totalCorrect = 0;
         totalQuestions = questions.size();
         Question firstQuestion = chooseNewQuestion();
@@ -161,15 +182,24 @@ public class MainActivity extends AppCompatActivity {
         currentQuestionIndex = newQuestionNumber;
         return questions.get(currentQuestionIndex);
     }
-    // Convenience method for calling curretly selected question object
+    // Call the question displayed for player
     public Question getCurrentQuestion() {
         Question currentQuestion = questions.get(currentQuestionIndex);
         return currentQuestion;
     }
 
+    // Submit answer and change to next question
+
     public void onAnswerSubmission() {
         Question currentQuestion = getCurrentQuestion();
-        if ( currentQuestion.isCorrect() ) {
+        if (currentQuestion.playerAnswer == -1) {
+            AlertDialog.Builder selectAnAnswerBuilder = new AlertDialog.Builder(MainActivity.this);
+            selectAnAnswerBuilder.setCancelable(true);
+            selectAnAnswerBuilder.setTitle("Please choose an answer.");
+            selectAnAnswerBuilder.setMessage("Click outside the box to select an answer.");
+            selectAnAnswerBuilder.create().show();
+            return ;
+        } else if ( currentQuestion.isCorrect() ) {
             totalCorrect ++;
         }
         questions.remove(currentQuestion);
@@ -178,8 +208,18 @@ public class MainActivity extends AppCompatActivity {
         if (questions.size() == 0) {
             String gameOverMan = getGameOverMessage(totalCorrect, totalQuestions);
 
-            // TODO 5-D: Show a popup instead
-            System.out.println(gameOverMan);
+            // Game over message using an Alert
+                AlertDialog.Builder gameOverDialogueBuilder = new AlertDialog.Builder(MainActivity.this);
+                gameOverDialogueBuilder.setCancelable(false);
+                gameOverDialogueBuilder.setTitle("Game Over!");
+                gameOverDialogueBuilder.setMessage(gameOverMan);
+                gameOverDialogueBuilder.setPositiveButton("Play Again!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startNewGame();
+                    }
+                });
+                gameOverDialogueBuilder.create().show();
             startNewGame();
         } else {
             chooseNewQuestion();
